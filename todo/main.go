91a8b2e0d2779f45b51bf6789362f0e3dc2c3933
Louis des Landes <louis.deslandes@fleet.space>
2019-11-03 14:07:26 +1100

    Add some comments

--- todo/main.go
+++ todo/main.go
@@ -31,14 +31,15 @@ func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (Response, 
 	message = req.Path
 	statusCode := 200
 
+	// Could use a third party routing library at this point, but being hacky for now
 	items := strings.Split(req.Path, "/")
 	var item string
 	if len(items) > 1 {
 		item = strings.Join(items[2:], "/")
 	}
 
+	// If we actually have an action to take
 	if len(items) >= 1 {
-
 		switch items[1] {
 		case "list":
 			items, err := List()
@@ -49,6 +50,7 @@ func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (Response, 
 				message = strings.Join(items, "\n")
 			}
 		case "add":
+			// Should probably be doing this on PUT or POST only
 			err := Add(item)
 			if err != nil {
 				statusCode = 500
@@ -58,6 +60,7 @@ func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (Response, 
 			}
 
 		case "complete":
+			// Should only be doing this on POST, but demo
 			err := Complete(item)
 			if err != nil {
 				statusCode = 500
@@ -106,11 +109,13 @@ func List() ([]string, error) {
 
 	results := []string{}
 	for _, row := range scanOutput.Items {
+		// If the dynamodb item might have something not a string this would have to change
 		results = append(results, *row[todoKey].S)
 	}
 	return results, nil
 }
 
+// Add an item to the todo list
 func Add(v string) error {
 	mySession, err := session.NewSession()
 	if err != nil {
@@ -131,6 +136,7 @@ func Add(v string) error {
 	return err
 }
 
+// Complete an item in the todo list (just delete it at this stage...)
 func Complete(v string) error {
 	mySession, err := session.NewSession()
 	if err != nil {
